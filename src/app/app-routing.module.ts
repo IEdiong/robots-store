4593593c475c0a5e4ebef5888cc664a2ae893dd3
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-07-21 18:03:24 +0100

    add sign in page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CatalogComponent } from './catalog/catalog.component';
 import { CartComponent } from './cart/cart.component';
 import { HomeComponent } from './home/home.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent, title: "Home | Joe's Robot Shop" },
@@ -12,6 +13,11 @@ const routes: Routes = [
     title: "Catalog | Joe's Robot Shop",
   },
   { path: 'cart', component: CartComponent, title: "Cart | Joe's Robot Shop" },
+  {
+    path: 'sign-in',
+    component: SignInComponent,
+    title: "Sign In | Joe's Robot Shop",
+  },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
