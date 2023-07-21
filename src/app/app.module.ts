8b886bb3860ce6d743a317c29833e55e26087878
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-07-21 16:50:24 +0100

    add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { SiteHeaderComponent } from './site-header/site-header.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -15,8 +17,9 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     CatalogComponent,
     SiteHeaderComponent,
     ProductDetailsComponent,
+    CartComponent,
   ],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
